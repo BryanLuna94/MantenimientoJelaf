465287c1ff7c89670867f82a1eef2217c3c12d23
Julio Luna <julio.luna@mdp.com.pe>
2020-03-20 10:01:40 -0500

    SESION expire

--- Template.WebApp/Helpers/SessionExpire.cs
+++ Template.WebApp/Helpers/SessionExpire.cs
@@ -3,6 +3,30 @@ using System.Web.Routing;
 
 namespace Mantenimiento.WebApp.Helpers
 {
+    public class SessionExpireFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Validar la información que se encuentra en la sesión.
+            if (DataSession.UserLoggedIn == null || DataSession.UserLoggedIn.Nombre == null)
+            {
+                // Si la información es nula, redireccionar a 
+                // página de error u otra página deseada.
+                var ruta = filterContext.RequestContext.HttpContext.Request.Url.AbsolutePath;
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary
+                   {
+                           { "action", "Login" },
+                           { "controller", "Autenticacion" },
+                           { "Area", string.Empty },
+                           { "returnUrl", ruta }
+                   });
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+
     public class SessionExpire : ActionFilterAttribute
     {
         public override void OnActionExecuting(ActionExecutingContext filterContext)
